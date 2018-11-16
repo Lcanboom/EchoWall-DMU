814f8b3dbe04b3388c382cdf33335ee16ec5b516
Lcanboom <38036076+Lcanboom@users.noreply.github.com>
2018-11-16 23:16:34 +0800

    Update comment.js

--- echowall/controller/comment.js
+++ echowall/controller/comment.js
@@ -85,4 +85,70 @@ router.post('/add',function(req, res, next){
 	})
 })
 
+router.post('/like',function(req, res, next){
+	var openid = req.body.openid;
+	var sk = req.body.sk;
+	var commentid = req.body.commentid;
+	var flag = req.body.flag;
+	var time = req.body.time;
+	var sqlArray = [];
+	var pool = database.connection();
+	var	sql_add_Commentlike;
+	wechatCommunicate.verifySk(sk).then(isVerified => {
+		if (isVerified) {
+			wechatCommunicate.getUserInfo(openid).then(result => {
+				console.log(result);
+				if (Array.isArray(result) && result.length === 0){
+					error = {
+				    	'status': 500,
+				    	'error': 'openid is not exits',						
+					}
+					res.json(error);
+				}					
+				else {
+					// 事件的事务组成
+					var sql_add_userAction = "insert INTO userAction set ?";
+					param = {
+						'userId': result[0].id, 
+						'commentId': commentid,
+						'actionType': 'like',
+						'time': time
+					};
+					sqlArray.push(database.getSqlParamEntity(sql_add_userAction, param));
+					
+					if (flag) 
+						sql_Commentlike = "update comment set likeNum = likeNum + 1 where ?";
+					else
+						sql_Commentlike = "update comment set likeNum = likeNum - 1 where ?";						
+					param = { 'id': commentid };
+					sqlArray.push(database.getSqlParamEntity(sql_Commentlike, param));
+
+					database.transaction(pool, sqlArray).then( (err, result) => {
+						if (err) 
+							res.json(err);
+						else
+							res.json(result);
+					}, (err) => {
+						res.json({
+							'status': 500,
+							'message': 'rollback errorr'
+						})
+					})
+				}
+			}, (error) => {
+				res.json(error);
+			})
+		}
+		else {
+				error = {
+			    	'status': 500,
+			    	'error': 'sk is not exits',						
+				}
+				res.json(error);
+		}
+	}, (error) => {
+		res.json(error);
+	})
+})
+
 module.exports = router;
