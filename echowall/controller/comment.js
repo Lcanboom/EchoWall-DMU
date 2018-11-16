1829a9ad44ad0f1b61598f7b00bd708a74886150
Lcanboom <38036076+Lcanboom@users.noreply.github.com>
2018-11-16 22:14:27 +0800

    Update comment.js

--- echowall/controller/comment.js
+++ echowall/controller/comment.js
@@ -36,7 +36,7 @@ router.post('/add',function(req, res, next){
 					// 事件的事务组成
 					var sql_add_comment = "insert INTO comment set ?";
 					param = {
-						'userId': result.id, 
+						'userId': result[0].id, 
 						'echoId': echoid,
 						'content': content,
 						'time': time
@@ -45,7 +45,7 @@ router.post('/add',function(req, res, next){
 					
 					var sql_add_userAction = "insert INTO userAction set ?";
 					param = {
-						'userId': result['id'], 
+						'userId': result[0].id, 
 						'echoId': echoid,
 						'actionType': 'commit',
 						'time': time
