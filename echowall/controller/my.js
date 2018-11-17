b62ab6201a6acd43f379a3037312751aa7f50de6
Lcanboom <38036076+Lcanboom@users.noreply.github.com>
2018-11-17 10:06:36 +0800

    Update my.js

--- echowall/controller/my.js
+++ echowall/controller/my.js
@@ -14,23 +14,6 @@ var sql;
 /*
 * 获取某 user 的评论列表
 */
-router.get('/comment',function(req, res, next){
-	var connection = database.connection();
-	var echoId = req.query.echoid;
-	page = req.query.page;
-	start = (page - 1) * per_page_count;
-	sql = "Select * from comment" + "ORDER BY time DESC limit " + start + ", " + per_page_count;
-	database.query(connection, echoId, sql).then((data) => {
-		if (data)
-			res.jsonp(data);
-		else
-			res.jsonp({
-		    	'status': "500",
-		    	'message':"query error",
-			})
-	}, (err) => {
-			res.jsonp(err);	
-	});
-})
+
 
 module.exports = router;
