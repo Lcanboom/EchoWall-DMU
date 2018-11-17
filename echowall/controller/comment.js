677cf68cfae91354a6460ee52974c3193d82b5bc
Lcanboom <38036076+Lcanboom@users.noreply.github.com>
2018-11-17 10:48:34 +0800

    Update comment.js

--- echowall/controller/comment.js
+++ echowall/controller/comment.js
@@ -27,7 +27,6 @@ router.post('/add',function(req, res, next){
 	var echoid = req.body.echoid;
 	var content = req.body.content;
 	var time = req.body.time;
-	var pool = database.connection();
 	// 事务组成
 	var sql_add_comment = "insert INTO comment set ?";
 	param = {
@@ -57,142 +56,73 @@ router.post('/add',function(req, res, next){
 	}, (error) => {
 		res.json(error);
 	});
-
-
 })
 
 router.post('/like',function(req, res, next){
+	var userId = req.body.userid;
 	var openid = req.body.openid;
 	var sk = req.body.sk;
 	var commentid = req.body.commentid;
 	var flag = req.body.flag;
-	console.log(flag);
 	var time = req.body.time;
-	var pool = database.connection();
 	var	sql_Commentlike;
 	var actionType;
-	wechatCommunicate.verifySk(sk).then(isVerified => {
-		if (isVerified) {
-			wechatCommunicate.getUserInfo(openid).then(result => {
-				console.log(result);
-				if (Array.isArray(result) && result.length === 0){
-					error = {
-				    	'status': 500,
-				    	'error': 'openid is not exits',						
-					}
-					res.json(error);
-				}					
-				else {
-					// 事件的事务组成
-					if (flag) {
-						actionType = 'like_do';
-						sql_Commentlike = "update comment set likeNum = likeNum + 1 where ?";						
-					}
-					else{
-						actionType = 'like_undo';						
-						sql_Commentlike = "update comment set likeNum = likeNum - 1 where ?";												
-					}
 
-					var sql_add_userAction = "insert INTO userAction set ?";
-					param = {
-						'userId': result[0].id, 
-						'commentId': commentid,
-						'actionType': actionType,
-						'time': time
-					};
-					sqlArray.push(database.getSqlParamEntity(sql_add_userAction, param));
+	if (flag) {
+		actionType = 'like_do';
+		sql_Commentlike = "update comment set likeNum = likeNum + 1 where ?";						
+	}
+	else{
+			actionType = 'like_undo';						
+			sql_Commentlike = "update comment set likeNum = likeNum - 1 where ?";												
+	}
+
+	var sql_add_userAction = "insert INTO userAction set ?";
+	param = {
+				'userId': userId, 
+				'commentId': commentid,
+				'actionType': actionType,
+				'time': time
+			};
+	sqlArray.push(database.getSqlParamEntity(sql_add_userAction, param));
 										
-					param = { 'id': commentid };
-					sqlArray.push(database.getSqlParamEntity(sql_Commentlike, param));
+	param = { 'id': commentid };
+	sqlArray.push(database.getSqlParamEntity(sql_Commentlike, param));
 
-					database.transaction(pool, sqlArray).then( (err, result) => {
-						if (err) 
-							res.json(err);
-						else
-							res.json(result);
-						sqlArray = [];
-					}, (err) => {
-						res.json({
-							'status': 500,
-							'message': 'rollback errorr'
-						})
-					})
-				}
-			}, (error) => {
-				res.json(error);
-			})
-		}
-		else {
-				error = {
-			    	'status': 500,
-			    	'error': 'sk is not exits',						
-				}
-				res.json(error);
-		}
+	userCommon.userTransaction(sk, openid, sqlArray).then((result) => {
+		if (result) 
+			res.json(result);
 	}, (error) => {
 		res.json(error);
-	})
+	});
 })
 
 router.post('/dislike',function(req, res, next){
+	var userId = req.body.userid;
 	var openid = req.body.openid;
 	var sk = req.body.sk;
 	var commentid = req.body.commentid;
 	var time = req.body.time;
-	var pool = database.connection();
-	wechatCommunicate.verifySk(sk).then(isVerified => {
-		if (isVerified) {
-			wechatCommunicate.getUserInfo(openid).then(result => {
-				console.log(result);
-				if (Array.isArray(result) && result.length === 0){
-					error = {
-				    	'status': 500,
-				    	'error': 'openid is not exits',						
-					}
-					res.json(error);
-				}					
-				else {
-					// 事件的事务组成
-					var sql_dislike_userAction = "insert INTO userAction set ?";
-					param = {
-						'userId': result[0].id, 
-						'commentId': commentid,
-						'actionType': 'dislike',
-						'time': time
-					};
-					sqlArray.push(database.getSqlParamEntity(sql_dislike_userAction, param));
-					
-					var	sql_commentDislike = "update comment set dislikeNum = dislikeNum + 1 where ?";
-					param = { 'id': commentid };
-					sqlArray.push(database.getSqlParamEntity(sql_commentDislike, param));
 
-					database.transaction(pool, sqlArray).then( (err, result) => {
-						if (err) 
-							res.json(err);
-						else
-							res.json(result);
-						sqlArray = [];
-					}, (err) => {
-						res.json({
-							'status': 500,
-							'message': 'rollback errorr'
-						})
-					})
-				}
-			}, (error) => {
-				res.json(error);
-			})
-		}
-		else {
-				error = {
-			    	'status': 500,
-			    	'error': 'sk is not exits',						
-				}
-				res.json(error);
-		}
+	var sql_dislike_userAction = "insert INTO userAction set ?";
+	param = {
+				'userId': userId, 
+				'commentId': commentid,
+				'actionType': 'dislike',
+				'time': time
+			};
+	sqlArray.push(database.getSqlParamEntity(sql_dislike_userAction, param));
+					
+	var	sql_commentDislike = "update comment set dislikeNum = dislikeNum + 1 where ?";
+	param = { 'id': commentid };
+	sqlArray.push(database.getSqlParamEntity(sql_commentDislike, param));
+
+	userCommon.userTransaction(sk, openid, sqlArray).then((result) => {
+		if (result) 
+			res.json(result);
 	}, (error) => {
 		res.json(error);
-	})
+	});
 })
 
 /*
