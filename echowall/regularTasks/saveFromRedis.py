738f9e9f1bf3e02b4d774e518864a7bf2bb2c9b8
Lcanboom <38036076+Lcanboom@users.noreply.github.com>
2018-11-18 17:28:29 +0800

    Update saveFromRedis.py

--- echowall/regularTasks/saveFromRedis.py
+++ echowall/regularTasks/saveFromRedis.py
@@ -10,8 +10,8 @@ pool = redis.ConnectionPool(host='localhost',port=6379,db=0,password='echowall')
 r = redis.StrictRedis(connection_pool=pool)
  
 def write_to_file(content):
-	time = time = datetime.datetime.now()
-	with open('/home/Carmelo/Lcanboom/echowall/regularTasks/insertlog/{time}.txt'.format(time=time), 'a', encoding='utf-8') as f:
+	time = datetime.datetime.now().date()
+	with open("/home/Carmelo/Lcanboom/echowall/regularTasks/insertlog/{time}.txt".format(time=time), 'a', encoding='utf-8') as f:
 		f.write(content + '\n')
 
 def save_to_mysql(db, parms):
