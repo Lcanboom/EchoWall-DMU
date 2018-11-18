2117d17d29e5ed4ce319756fc6f833fcde633659
Lcanboom <38036076+Lcanboom@users.noreply.github.com>
2018-11-18 13:18:46 +0800

    Update hot.js

--- echowall/controller/hot.js
+++ echowall/controller/hot.js
@@ -16,16 +16,29 @@ function arrayToString(array) {
 
 // 获取热门的回音壁信息（标准：两周时间内的浏览量降序排序）
 router.get('/byview', function(req, res){
-	// 获取缓存中有序集合的 id 列表
 	var connection = database.connection();
 	var redis_client = database.redis_connection();
-	database.redis_zrevrangebyscore(redis_client, zset).then((result) => {
-		result = arrayToString(result);
-		console.log(result);
-		res.jsonp(result);
+	page = req.body.page;
+	start = (page - 1) * per_page_count;
+	// 获取缓存中有序集合的 id 列表
+	database.redis_zrevrangebyscore(redis_client, zset).then((hotlist) => {
+		hotlist = arrayToString(hotlist);
+		sql = "select * from echowall where id in (?) order by FIELD(id, ?) LIMIT " + start + ', ' + per_page_count; 
+		database.query(connection, hotlist, sql).then((data) => {
+			if (data)
+				res.jsonp(data);
+			else
+				res.jsonp({
+			    	'status': "500",
+			    	'message':"query error",
+				})
+		}, (err) => {
+				res.jsonp(err);	
+		});	
+	}, (err) => {
+		res.jsonp(err);	
 	})
 	redis_client.quit();
 });
-
 
 module.exports = router;
