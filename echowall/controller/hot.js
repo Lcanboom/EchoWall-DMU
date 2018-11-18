f4753b96ab9931efc6379aa46fe935715e41fc97
Lcanboom <38036076+Lcanboom@users.noreply.github.com>
2018-11-18 13:11:10 +0800

    Update hot.js

--- echowall/controller/hot.js
+++ echowall/controller/hot.js
@@ -8,14 +8,21 @@ var page;
 var start;
 var sql;
 
+function arrayToString(array) {
+	return array.reduce((acc, item) => {
+			return  acc + "," + '\'' + item + '\'';
+			}, '\'' + array[0] + '\'');
+}
+
 // 获取热门的回音壁信息（标准：两周时间内的浏览量降序排序）
 router.get('/byview', function(req, res){
 	// 获取缓存中有序集合的 id 列表
 	var connection = database.connection();
 	var redis_client = database.redis_connection();
 	database.redis_zrevrangebyscore(redis_client, zset).then((result) => {
-		console.log(result.join());
-		res.jsonp(result.join());
+		result = arrayToString(array);
+		console.log(result);
+		res.jsonp(result);
 	})
 	redis_client.quit();
 });
