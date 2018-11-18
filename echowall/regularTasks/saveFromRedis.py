b04a58b446f8c2e199e8eb3cab5c80d83ecd8302
Lcanboom <38036076+Lcanboom@users.noreply.github.com>
2018-11-18 15:04:08 +0800

    Update saveFromRedis.py

--- echowall/regularTasks/saveFromRedis.py
+++ echowall/regularTasks/saveFromRedis.py
@@ -1,8 +1,8 @@
 # -*- encoding: UTF-8 -*-
 import redis
-pool = redis.ConnectionPool(host='localhost',port=6379,db=0)
+pool = redis.ConnectionPool(host='localhost',port=6379,db=0,auth={auth_pass: 'echowall'})
 r = redis.StrictRedis(connection_pool=pool)
 
 keys = r.keys()
-print type(keys)
+print (type(keys))
 print (keys)
