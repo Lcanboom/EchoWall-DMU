1ea34cfdb6fb685e171e157667b54e78409c5370
Lcanboom <38036076+Lcanboom@users.noreply.github.com>
2018-11-18 14:07:05 +0800

    update

--- echowall/controller/function/common.js
+++ echowall/controller/function/common.js
@@ -2,7 +2,9 @@
 *	特殊的公共处理
 */
 
-
+/*
+*	将数组['1','2'] => "'1','2'"
+*/
 function arrayToString(array) {
 	array[0] = '\'' + array[0] + '\'';
 	return array.reduce((acc, item) => {
--- echowall/controller/hot.js
+++ echowall/controller/hot.js
@@ -20,9 +20,9 @@ router.get('/byview', function(req, res){
 		hotlist = common.arrayToString(hotlist);
 		console.log(hotlist);
 		sql = "SELECT id, title, box, date_format(time, '%Y-%m-%d %H:%i:%s') time from echowall where id in (" + hotlist +
-		") order by FIELD(id,?) LIMIT " + start + ', ' + per_page_count; 
+		") order by FIELD(id, "+ hotlist + ") LIMIT " + start + ', ' + per_page_count; 
 		console.log(sql);
-		database.query(pool, hotlist, sql).then((data) => {
+		database.query(pool, null, sql).then((data) => {
 			if (data)
 				res.jsonp(data);
 			else
