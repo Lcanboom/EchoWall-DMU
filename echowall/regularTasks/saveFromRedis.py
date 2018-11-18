a6ef41159977e0fc9a11d72b1b09069380fb6f4c
Lcanboom <38036076+Lcanboom@users.noreply.github.com>
2018-11-18 16:30:23 +0800

    Update saveFromRedis.py

--- echowall/regularTasks/saveFromRedis.py
+++ echowall/regularTasks/saveFromRedis.py
@@ -1,32 +1,30 @@
 # -*- encoding: UTF-8 -*-
 import redis
 import pymysql
+db = pymysql.connect("localhost","root","521Loli","test_echo")
 pool = redis.ConnectionPool(host='localhost',port=6379,db=0,password='echowall')
 r = redis.StrictRedis(connection_pool=pool)
-db = pymysql.connect("localhost","root","521Loli","test_echo")
-
-# 使用cursor()方法获取操作游标 
-cursor = db.cursor()
  
-# SQL 更新语句
-sql = "UPDATE echowall SET viewCount = viewCount + %s WHERE id = %s" 
+def save_to_mysql(db, parms):
+	# 使用cursor()方法获取操作游标 
+	cursor = db.cursor()
+	sql = "UPDATE echowall SET viewCount = viewCount + %s WHERE id = %s" 
+	try:
+	   # 执行sql语句
+	   reCount = cursor.execute(sql, (3, '66707131'))
+	   print(reCount)
+	   # 执行sql语句
+	   db.commit()
+	except:
+		# 发生错误时回滚
+		db.rollback()
 
-try:
-   # 执行sql语句
-   reCount = cursor.execute(sql, (3, '66707131'))
-   print(reCount)
-   # 执行sql语句
-   db.commit()
-except:
-	# 发生错误时回滚
-	db.rollback()
- 
-# 关闭数据库连接
-db.close()
+def get_from_redis(client):
+	view_last_twoWeek = client.zrange("view_last_twoWeek", 0, -1, withscores=True)
+	for item in view_last_twoWeek:
+		print(item)
+	pass
 
-keys = r.keys()
-print(type(keys))
-print(keys)
-view_last_twoWeek = r.zrange("view_last_twoWeek", 0, -1, withscores=True)
-print(type(view_last_twoWeek))
-print(view_last_twoWeek) 
+
+def main():
+	get_from_redis(r)
