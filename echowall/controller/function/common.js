feb30b2a1268aef10297fb02a434ccaa0a3d7cb4
Lcanboom <38036076+Lcanboom@users.noreply.github.com>
2018-11-18 13:58:50 +0800

    Update common.js

--- echowall/controller/function/common.js
+++ echowall/controller/function/common.js
@@ -6,7 +6,7 @@
 function arrayToString(array) {
 	return array.reduce((acc, item) => {
 			return  acc + "," + '\'' + item + '\'';
-			}, '\'' + array[0] + '\'');
+			});
 }
 
 exports.arrayToString = arrayToString;
