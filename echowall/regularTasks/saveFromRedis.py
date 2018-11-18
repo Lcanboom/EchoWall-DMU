46df9ea083ad3b9a6d17ddff4cdf5e45a082b9d9
Lcanboom <38036076+Lcanboom@users.noreply.github.com>
2018-11-18 15:38:39 +0800

    Update saveFromRedis.py

--- echowall/regularTasks/saveFromRedis.py
+++ echowall/regularTasks/saveFromRedis.py
@@ -1,7 +1,21 @@
 # -*- encoding: UTF-8 -*-
 import redis
+import pymysql
 pool = redis.ConnectionPool(host='localhost',port=6379,db=0,password='echowall')
 r = redis.StrictRedis(connection_pool=pool)
+db = pymysql.connect("localhost","root","521Loli","test_echo")
+cursor = db.cursor()
+
+# 使用 execute()  方法执行 SQL 查询 
+cursor.execute("SELECT VERSION()")
+ 
+# 使用 fetchone() 方法获取单条数据.
+data = cursor.fetchone()
+ 
+print ("Database version : %s " % data)
+ 
+# 关闭数据库连接
+db.close()
 
 keys = r.keys()
 print(type(keys))
