18c60e6c1b3a61396a454ab8cc654544054b9107
Lcanboom <38036076+Lcanboom@users.noreply.github.com>
2018-11-22 15:11:16 +0800

    Update test.py

--- echowall/regularTasks/script/test.py
+++ echowall/regularTasks/script/test.py
@@ -19,8 +19,8 @@ def save_to_mysql(db, parms):
 	# 获取昨天的数据
 	yesterday = getYesterday()
 	data = read_yesterday_csv(yesterday)
-	if parms[1] in data.keys() and parms[0] > data[parms[1]]:	# 排除两周后缓存数据清 0
-		increaseCount = parms[0] - data[parms[1]]				# 今天 - 昨天 = 增量
+	if parms[1] in data.keys() and parms[0] > float(data[parms[1]]):	# 排除两周后缓存数据清 0
+		increaseCount = parms[0] - float(data[parms[1]])				# 今天 - 昨天 = 增量
 	else:
 		increaseCount = parms[0]
 	# 使用cursor()方法获取操作游标 
